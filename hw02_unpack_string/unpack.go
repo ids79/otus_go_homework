c2f6558e0cd1ca897b6eff0cdffc6d40ed57bb18
Иванов Дмитрий Сергеевич <DIvanov@elga.ru>
2022-12-09 12:26:14 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,45 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var bilder strings.Builder
+	lastSumbol := ' '
+	screening := false
+	for _, r := range str {
+		if lastSumbol != '\\' && !screening && (lastSumbol == ' ' || unicode.IsDigit(lastSumbol)) && unicode.IsDigit(r) {
+			return "", ErrInvalidString
+		}
+		if unicode.IsLetter(lastSumbol) || screening {
+			if unicode.IsDigit(r) {
+				c, _ := strconv.Atoi(string(r))
+				bilder.Grow(c)
+				bilder.WriteString(strings.Repeat(string(lastSumbol), c))
+			} else {
+				bilder.WriteRune(lastSumbol)
+			}
+			screening = false
+			lastSumbol = r
+			continue
+		}
+		if lastSumbol == '\\' {
+			screening = true
+		}
+		lastSumbol = r
+	}
+	if lastSumbol == '\\' {
+		return "", ErrInvalidString
+	}
+	if unicode.IsLetter(lastSumbol) || screening {
+		bilder.Grow(1)
+		bilder.WriteRune(lastSumbol)
+	}
+
+	return bilder.String(), nil
 }

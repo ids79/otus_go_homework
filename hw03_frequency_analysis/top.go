375a4694e74807e8435e23802eca1e7483e29f51
Иванов Дмитрий Сергеевич <DIvanov@elga.ru>
2022-12-15 16:42:45 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,56 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"math"
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type TopWords struct {
+	word   string
+	number int
+}
+
+var TaskWithAsteriskIsCompleted = false
+
+func getFunc(top []TopWords) func(i, j int) bool {
+	return func(i, j int) bool {
+		if top[i].number == top[j].number {
+			return strings.Compare(top[i].word, top[j].word) == -1
+		}
+		return top[i].number > top[j].number
+	}
+}
+
+var rC = regexp.MustCompile(`[,|.|!|?|"|:|;]`)
+var top []TopWords
+
+func Top10(str string) []string {
+	if TaskWithAsteriskIsCompleted {
+		str = strings.ToLower(str)
+		str = rC.ReplaceAllString(str, "")
+	}
+	words := strings.Fields(str)
+	m := make(map[string]int)
+	for _, word := range words {
+		if TaskWithAsteriskIsCompleted && word == "-" {
+			continue
+		}
+		m[word]++
+	}
+	for word, n := range m {
+		t := TopWords{word: word, number: n}
+		top = append(top, t)
+	}
+	sort.Slice(top, getFunc(top))
+	top10Len := int(math.Min(float64(10), float64(len(top))))
+	top10 := make([]string, top10Len)
+	for i := 0; i < 10; i++ {
+		if len(top) <= i {
+			break
+		}
+		top10[i] = top[i].word
+	}
+	return top10
 }

fcd9f9333b29fbae3341ae1bb7610e8c4d1e6f99
ids79 <ids79@yandex.ru>
2023-04-10 15:12:12 +0000

    HW12 is completed 14

--- hw12_13_14_15_calendar/internal/storage/sql/storage.go
+++ hw12_13_14_15_calendar/internal/storage/sql/storage.go
@@ -9,7 +9,7 @@ import (
 	"github.com/ids79/otus_go_homework/hw12_13_14_15_calendar/internal/logger"
 	"github.com/ids79/otus_go_homework/hw12_13_14_15_calendar/internal/storage/types"
 	"github.com/jmoiron/sqlx"
-	"github.com/pressly/goose/v3"
+	goose "github.com/pressly/goose/v3"
 	uuid "github.com/satori/go.uuid"
 )
 
